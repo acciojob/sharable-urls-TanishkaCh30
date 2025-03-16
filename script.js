f50cf18c469af09e936a3fc8fc3530c1869e1d80
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-16 21:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+let btn = document.getElementById("button");
+let url = document.getElementById("url");
+let name= document.getElementById("name");
+let year =  document.getElementById("year");
+
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	if(name.value && year.value){
+		url.innerText = `https://localhost:8080/?name=${name.value}&year=${year.value}`;
+	}
+	else if(year.value){
+	url.innerText = `https://localhost:8080/?year=${year.value}`;
+	}
+	else if(name.value){
+	url.innerText = `https://localhost:8080/?name=${name.value}`;
+	}
+})
